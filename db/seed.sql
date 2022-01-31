d15f0b92aa257034f296c462f8016d708c3837a5
helony <91100090+helony@users.noreply.github.com>
2022-01-31 16:14:38 -0500

    Add files via upload

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO voitures
+
+VALUES('Audi',2020,'A4',265,'Rouge'),
+('Mercedes-Benz',2021,'C200',165,'Bleu'),
+('Seat',2022,'Leon FR',300,'Rouge'),
+('BMW',2021,'M5',455,'Blanc'),
+('Audi',2022,'S6',395,'Noir');
