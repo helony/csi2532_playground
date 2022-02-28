3b2086796d170306c5777688a5bcf473169c7a44
helony <91100090+helony@users.noreply.github.com>
2022-02-28 15:19:10 -0500

    Update 20220228135900-create-athletes.sql

--- migrations/20220228135900-create-athletes.sql
+++ migrations/20220228135900-create-athletes.sql
@@ -8,4 +8,9 @@ CREATE TABLE athletes (
   identified_gender varchar(6),
   PRIMARY KEY (id)
 );
+INSERT INTO athletes (id, name, identified_gender, dob) VALUES
+(1, 'Andrew', 'm', '1975-12-01'),
+(2, 'Ayana', 'F', '1998-06-11'),
+(3, 'Hayden', 'm', '1996-07-24'),
+(4, 'August', 'm', '1999-09-09');
 
